4ba7f1b2a35c66ca1cad7036dedf2c64b8c69ecf
m0rtal <m0rtal@mail.ru>
2024-08-02 15:36:54 +0300

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.DecimalField(max_digits=10, decimal_places=0, default=0)
