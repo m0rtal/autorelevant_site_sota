1e6f1e6acee3df702489ba5c14138cd1f493aa57
m0rtal <m0rtal@mail.ru>
2024-08-06 10:23:19 +0300

    Формы выбора регионов

--- avtorelevant_site/settings.py
+++ avtorelevant_site/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# URL prefix for static files
+STATIC_URL = '/static/'
 
+# Additional locations of static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # Add other directories here if necessary
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
