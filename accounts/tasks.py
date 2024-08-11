dcbddab7465f5dbc42f525924ca520dcfaf66aba
m0rtal <m0rtal@mail.ru>
2024-08-11 15:31:11 +0300

    Semaphore

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -16,39 +16,43 @@ async def save_uploaded_file(uploaded_file):
     return await asyncio.to_thread(uploaded_file.save)
 
 
-async def fetch_yandex(session, url, search_string, yandex_region):
-    async with session.get('http://176.9.48.55:5000/process-url/', params={
-        'url': url,
-        'search_string': search_string,
-        'region': yandex_region,
-        'return_as_json': 1
-    }) as response:
-        return await response.json(encoding='utf-8')
+async def fetch_yandex(session, url, search_string, yandex_region, semaphore):
+    async with semaphore:
+        async with session.get('http://176.9.48.55:5000/process-url/', params={
+            'url': url,
+            'search_string': search_string,
+            'region': yandex_region,
+            'return_as_json': 1
+        }) as response:
+            # r = await response.json(encoding='utf-8')
+            return await response.json(encoding='utf-8')
 
 
-async def fetch_google(session, url, search_string, google_region):
-    async with session.get('http://176.9.48.55:5000/search-google/', params={
-        'url': url,
-        'search_string': search_string,
-        'location': google_region,
-        'domain': 'google.ru',
-        'return_as_json': 1
-    }) as response:
-        return await response.json(encoding='utf-8')
+async def fetch_google(session, url, search_string, google_region, semaphore):
+    async with semaphore:
+        async with session.get('http://176.9.48.55:5000/search-google/', params={
+            'url': url,
+            'search_string': search_string,
+            'location': google_region,
+            'domain': 'google.ru',
+            'return_as_json': 1
+        }) as response:
+            # r = await response.json(encoding='utf-8')
+            return await response.json(encoding='utf-8')
 
 
-async def process_row(session, row, google_region, yandex_region):
-    yandex_task = fetch_yandex(session, row['url'], row['search_string'], yandex_region)
-    google_task = fetch_google(session, row['url'], row['search_string'], google_region)
+async def process_row(session, row, google_region, yandex_region, semaphore):
+    yandex_task = fetch_yandex(session, row['url'], row['search_string'], yandex_region, semaphore)
+    google_task = fetch_google(session, row['url'], row['search_string'], google_region, semaphore)
 
     yandex_result, google_result = await asyncio.gather(yandex_task, google_task)
     yandex_result, google_result = json.loads(yandex_result), json.loads(google_result)
     yandex_result['url'] = row['url']
     yandex_result['search_string'] = row['search_string']
-    yandex_result['yandex_region'] = yandex_region  # Добавление региона Yandex в результат
+    yandex_result['yandex_region'] = yandex_region
     google_result['url'] = row['url']
     google_result['search_string'] = row['search_string']
-    google_result['google_region'] = google_region  # Добавление региона Google в результат
+    google_result['google_region'] = google_region
 
     return {
         'yandex_result': yandex_result,
@@ -68,10 +72,12 @@ async def process_file_async(uploaded_file_id, google_region, yandex_region):
 
         uploaded_file.status = 'Начата обработка'
 
+        semaphore = asyncio.Semaphore(1)
+
         async with aiohttp.ClientSession() as session:
             tasks = []
             for _, row in df.iterrows():
-                task = asyncio.ensure_future(process_row(session, row, google_region, yandex_region))
+                task = asyncio.ensure_future(process_row(session, row, google_region, yandex_region, semaphore))
                 tasks.append(task)
 
             results = await asyncio.gather(*tasks)
@@ -82,8 +88,14 @@ async def process_file_async(uploaded_file_id, google_region, yandex_region):
                 google_result = result['google_result']
 
                 combined_lsi = list(set(yandex_result.get('lsi', []) + google_result.get('lsi', [])))
-                combined_increase_freq = {k: max(yandex_result.get('увеличить частотность', {}).get(k, 0), google_result.get('увеличить частотность', {}).get(k, 0)) for k in set(yandex_result.get('увеличить частотность', {}).keys()).union(google_result.get('увеличить частотность', {}).keys())}
-                combined_decrease_freq = {k: min(yandex_result.get('уменьшить частотность', {}).get(k, 0), google_result.get('уменьшить частотность', {}).get(k, 0)) for k in set(yandex_result.get('уменьшить частотность', {}).keys()).union(google_result.get('уменьшить частотность', {}).keys())}
+                combined_increase_freq = {k: max(yandex_result.get('увеличить частотность', {}).get(k, 0),
+                                                 google_result.get('увеличить частотность', {}).get(k, 0)) for k in
+                                          set(yandex_result.get('увеличить частотность', {}).keys()).union(
+                                              google_result.get('увеличить частотность', {}).keys())}
+                combined_decrease_freq = {k: min(yandex_result.get('уменьшить частотность', {}).get(k, 0),
+                                                 google_result.get('уменьшить частотность', {}).get(k, 0)) for k in
+                                          set(yandex_result.get('уменьшить частотность', {}).keys()).union(
+                                              google_result.get('уменьшить частотность', {}).keys())}
 
                 yandex_links = yandex_result.get('обработанные ссылки', {})
                 google_links = google_result.get('обработанные ссылки', {})
